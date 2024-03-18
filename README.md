4149f8dcaa81a575bd493c85c2981c3a8ab25df5
Urtzi Aresti Santamaria - 290692 <urtzi@baturamobile.com>
2024-03-18 11:13:14 +0100

    draw route

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 npm run build-deploy to generate prod app and deploy to firebase hosting
 ##BREAK DEJAR EN RAMA APARTE stable-skeleton
 
+-Date selector (with range of dates) : range types entresemana, dia suelto, findes
+-Seats selector
+-Price Selector
+-
+
--- src/app/pages/publish/map/map.page.ts
+++ src/app/pages/publish/map/map.page.ts
@@ -9,9 +9,6 @@ import {Coordinates} from "../../../interfaces/Coordinates";
 import {TravelService} from "../../../services/travel.service";
 import {TravelModel} from "../../../models/travel-model";
 import {TrailService} from "../../../services/trail.service";
-import {Route, RouteData} from "../../../interfaces/route";
-import {GeoJSON} from "leaflet";
-import {GeoJsonObject} from "geojson";
 
 @Component({
   selector: 'app-map',
@@ -20,159 +17,145 @@ import {GeoJsonObject} from "geojson";
 })
 export class Map implements OnInit {
   leafletMap: any;
-  zoom: number = environment.mapZoom
+  zoom: number = environment.mapZoom;
   originAddress!: Address;
   destinyAddress!: Address;
-
   origin: boolean = false;
   destiny: boolean = false;
 
-  constructor(public locationService: LocationService,
-              private geocodingService: GeocodingService,
-              private route: ActivatedRoute,
-              private router: Router,
-              private travelService: TravelService,
-              private trailService: TrailService) {
+  constructor(
+    public locationService: LocationService,
+    private geocodingService: GeocodingService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private travelService: TravelService,
+    private trailService: TrailService
+  ) {
   }
-
 
   ngOnInit() {
     this.route.queryParams.subscribe(() => {
-
-        const currentNavigation = this.router.getCurrentNavigation();
-        if (currentNavigation && currentNavigation.extras?.state) {
-          if (currentNavigation.extras.state['originAddress']) {
-            this.originAddress = currentNavigation.extras.state['originAddress'] as Address;
-            this.origin = true;
-            this.destiny = false;
-            this.mapInit({lat: this.originAddress.lat, lng: this.originAddress.lon})
-          } else if (currentNavigation.extras.state['destinyAddress']) {
-            this.destinyAddress = currentNavigation.extras.state['destinyAddress'] as Address;
-            this.destiny = true;
-            this.origin = false
-            this.mapInit({lat: this.destinyAddress.lat, lng: this.destinyAddress.lon})
-          } else if (currentNavigation.extras.state['trailInfo']) {
-            //     this.getRouteTrail()
-            this.mapInit({
-              lat: environment.defaultPosition.latitude,
-              lng: environment.defaultPosition.lontitude
-            })
-          } else {
-            this.mapInit({
-              lat: environment.defaultPosition.latitude,
-              lng: environment.defaultPosition.lontitude
-            })
-          }
-        } else {
-          this.mapInit({
-            lat: environment.defaultPosition.latitude,
-            lng: environment.defaultPosition.lontitude
-          })
+      const currentNavigation = this.router.getCurrentNavigation();
+      if (currentNavigation && currentNavigation.extras?.state) {
+        const state = currentNavigation.extras.state;
+        if (state['originAddress']) {
+          this.originAddress = state['originAddress'] as Address;
+          this.origin = true;
+          this.destiny = false;
+          this.mapInit({lat: this.originAddress.lat, lng: this.originAddress.lon});
+        } else if (state['destinyAddress']) {
+          this.destinyAddress = state['destinyAddress'] as Address;
+          this.destiny = true;
+          this.origin = false;
+          this.mapInit({lat: this.destinyAddress.lat, lng: this.destinyAddress.lon});
+        } else if (state['trailInfo']) {
+          console.warn('Trail info');
+          this.destiny = false;
+          this.origin = false;
+          this.getRouteTrail();
         }
+      } else {
+        debugger
+        this.mapInit({
+          lat: environment.defaultPosition.latitude,
+          lng: environment.defaultPosition.lontitude,
+        });
       }
-    )
-    this.getRouteTrail()
+    });
   }
 
-
   getRouteTrail() {
+    const mockedTravelData: TravelModel = this.travelService.travelData;
+
     const travelData: TravelModel = this.travelService.travelData;
     console.warn(travelData)
 
     if (travelData.origin?.originCoords && travelData.destiny?.destinyCoords) {
-      this.trailService.getRouteTrail(travelData.origin.originCoords, travelData.destiny.destinyCoords).subscribe(res => {
-        console.warn(res)
+      this.trailService.getRouteTrail(travelData.origin.originCoords, travelData.destiny.destinyCoords).subscribe(trailData => {
+        this.drawRoute(trailData);
       })
-
     } else {
-      const mockedTravelData: TravelModel = this.travelService.travelData;
       mockedTravelData.origin = {
-        originCoords: {lat: 43.190913, lng: -3.199353}
-      }
+        originCoords: {lat: 43.190913, lng: -3.199353},
+      };
       mockedTravelData.destiny = {
-        destinyCoords: {lat: 43.211713, lng: -3.138442}
-      }
-      if (mockedTravelData.origin?.originCoords && mockedTravelData.destiny?.destinyCoords)
+        destinyCoords: {lat: 43.211713, lng: -3.138442},
+      };
+      if (mockedTravelData.origin?.originCoords && mockedTravelData.destiny?.destinyCoords) {
+        this.mapInit({lat: mockedTravelData.origin!.originCoords.lat, lng: mockedTravelData.origin!.originCoords.lng});
         this.trailService.getRouteTrail(mockedTravelData.origin.originCoords, mockedTravelData.destiny.destinyCoords).subscribe(trailData => {
-          this.drawRoute(trailData)
-        })
+          this.drawRoute(trailData);
+        });
+      }
     }
   }
 
-  drawRoute(trailData: RouteData) {
+  drawRoute(trailData: any) {
     const pathStyle = {
-      "color": "blue",
-      "weight": 4,
-      "opacity": 0.9
+      color: 'blue',
+      weight: 4,
+      opacity: 0.9,
     };
 
-    // @ts-ignore
     const geoJSON = L.geoJSON(trailData.routes[0].geometry, {
-      style: pathStyle
+      style: pathStyle,
     }).addTo(this.leafletMap);
     this.leafletMap.fitBounds(geoJSON.getBounds(), {
       padding: [10, 10],
-      animate: true
-    })
+      animate: true,
+    });
   }
 
   setPosition() {
+    if(this.origin || this.destiny){
+
+
     const leafletCenter = this.leafletMap.getCenter();
     this.geocodingService.getAddressFromLatLng(leafletCenter.lat, leafletCenter.lng).subscribe(addressResult => {
+      const travelData: TravelModel = this.travelService.travelData;
       if (this.origin) {
-        const travelData: TravelModel = this.travelService.travelData;
         travelData.origin = {
           name: addressResult.name,
           originPostalCode: addressResult.addressData.postcode,
-          originCoords: {lat: addressResult.lat, lng: addressResult.lon}
+          originCoords: {lat: addressResult.lat, lng: addressResult.lon},
         };
-        const navigationExtras: NavigationExtras = {
-          state: {
-            origin: true
-          }
-        };
-
-        this.router.navigateByUrl('home/publish/time-selector', navigationExtras);
       }
       if (this.destiny) {
-        const travelData: TravelModel = this.travelService.travelData;
         travelData.destiny = {
           name: addressResult.name,
           destinyPostalCode: addressResult.addressData.postcode,
-          destinyCoords: {lat: addressResult.lat, lng: addressResult.lon}
+          destinyCoords: {lat: addressResult.lat, lng: addressResult.lon},
         };
-        const navigationExtras: NavigationExtras = {
-          state: {
-            destiny: true
-          }
-        };
-        this.router.navigateByUrl('home/publish/time-selector', navigationExtras);
       }
-    })
+      const navigationExtras: NavigationExtras = {
+        state: {origin: this.origin, destiny: this.destiny},
+      };
+      this.router.navigateByUrl('home/publish/time-selector', navigationExtras);
+    });
+    }else {
+      console.warn("acept route")
+      this.router.navigateByUrl('home/publish/date-selector');
+    }
   }
 
   flyTo(userCoords: Coordinates): void {
-    if (userCoords.lat
-    ) {
-      console.warn(userCoords)
-      this.leafletMap.flyTo([userCoords.lat, userCoords.lng], 16, {
-        animate: true,
-        duration: 1
-      });
-    }
+    console.warn(userCoords);
+    this.leafletMap.flyTo([userCoords.lat, userCoords.lng], 16, {
+      animate: true,
+      duration: 1,
+    });
   }
 
   getCurrentPosition(): void {
     this.locationService.getCurrentPosition().subscribe({
-        next: (position) => {
-          this.leafletMap.setView([position.coords.latitude, position.coords.longitude], this.zoom);
-          this.flyTo({lat: position.coords.latitude, lng: position.coords.longitude});
-        },
-        error: (error) => {
-          console.error('Error getting current position:', error);
-        }
-      }
-    )
+      next: (position) => {
+        this.leafletMap.setView([position.coords.latitude, position.coords.longitude], this.zoom);
+        this.flyTo({lat: position.coords.latitude, lng: position.coords.longitude});
+      },
+      error: (error) => {
+        console.error('Error getting current position:', error);
+      },
+    });
   }
 
   setCenter(centerPosition: Coordinates, zoom: number) {
@@ -185,8 +168,9 @@ export class Map implements OnInit {
   }
 
   mapInit(centerPosition: Coordinates) {
-    this.leafletMap = new L.Map('leafletMap')
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(this.leafletMap);
+    console.warn("map init")
+    this.leafletMap = new L.Map('leafletMap');
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.leafletMap);
     this.leafletMap.setView([centerPosition.lat, centerPosition.lng], this.zoom);
     this.leafletMap.whenReady(() => {
       setTimeout(() => {
@@ -197,13 +181,12 @@ export class Map implements OnInit {
 
   watchPosition(): void {
     this.locationService.watchPosition().subscribe({
-        next: (position) => {
-          console.log('Updated Position:', position);
-        },
-        error: (error) => {
-          console.error('Error watching position:', error);
-        }
-      }
-    );
+      next: (position) => {
+        console.log('Updated Position:', position);
+      },
+      error: (error) => {
+        console.error('Error watching position:', error);
+      },
+    });
   }
 }
